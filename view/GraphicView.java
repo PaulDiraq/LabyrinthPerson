6e794636005b4476637deb27b89f248d8ea9f98d
lukas-dietsche <59418219+lukas-dietsche@users.noreply.github.com>
2023-06-27 23:18:57 +0200

    Update GraphicView.java

--- view/GraphicView.java
+++ view/GraphicView.java
@@ -78,6 +78,7 @@ public class GraphicView extends JPanel implements View {
 		    g.drawString(String.valueOf(this.numberOfHunters),bg.width/2,bg.height/2+30 );
 		}else {
 		    // Paint player
+			// Source: https://www.iconsdb.com/white-icons/happy-icon.html
 		    Image playerImage = loadImage("view\\face.png", fieldDimension.width, fieldDimension.height); // Load the image for the player
 			int playerX = playerPositionStored.getX() * fieldDimension.width;
 			int playerY = playerPositionStored.getY() * fieldDimension.height;
